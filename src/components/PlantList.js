19665993f73476da91581f90ad918ae3abf5ea5e
Cesar Hernandez <chema27281@gmail.com>
2022-10-20 11:18:01 -0400

    commit tree f8890e7a58c69b28516d0513e45c39a2ed663e93

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PlantCard from "./PlantCard";
-
 function PlantList() {
+  const [plants, setPlants] = useState([])
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(response => response.json())
+      .then(plants => setPlants(plants))
+  }, [])
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plants.map((plant) => {
+      return <PlantCard name={plant.name} key={plant.id}
+        image={plant.image} price={plant.price} />
+    }
+
+
+    )}
+
+
+
+    </ul>
   );
 }
 
